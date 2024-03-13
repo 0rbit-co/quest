e55bc472fd15a2deb4c0b5a4bd1bfefb34e9ed09
Manav <manav18gadhiya@gmail.com>
2024-03-13 16:08:20 +0530

    Fixed all the issues

--- price-feed-bot-Manav2808/PriceFeedBot.lua
+++ price-feed-bot-Manav2808/PriceFeedBot.lua
@@ -1,33 +1,22 @@
+-- Process ID: MD76snAyJJICvDt2rhhA68zIjPSIYJDKuyQ19yFiTGE
+
 _0RBIT = "WSXUI2JjYUldJ7CKq9wE1MGwXs-ldzlUlHOQszwQe0s"
 
 Handlers.add(
     "pingpong",
     Handlers.utils.hasMatchingTag("Action", "Get-Price"),
     function(msg)
-        ao.send({
-            Target = _0RBIT,
-            Action = "Get-Real-Data",
-            Url = 'https://api.coingecko.com/api/v3/simple/price?ids=' .. msg.Tags.ID .. '&vs_currencies=usd'
-        })
-        Handlers.utils.reply("Data Fetched")
-    end
-)
-
-Handlers.add(
-    "pingpong",
-    Handlers.utils.hasMatchingTag("Action", "Get-Prices"),
-    function(msg)
-        coin_ids = ''
+        local coin_ids = ''
         for i in string.gmatch(msg.Tags.IDS, "[^,]+") do
             i = i:match("^%s*(.-)%s*$")
             coin_ids = coin_ids .. i .. '%2C'
         end
-        print(coin_ids)
-        ao.send({
+
+        local response = ao.send({
             Target = _0RBIT,
             Action = "Get-Real-Data",
             Url = 'https://api.coingecko.com/api/v3/simple/price?ids=' .. coin_ids .. '&vs_currencies=usd'
         })
-        Handlers.utils.reply(coin_ids)(msg)
+        Handlers.utils.reply(response.Data)(msg)
     end
 )

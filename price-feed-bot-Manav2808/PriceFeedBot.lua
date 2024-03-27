5e3a593246ea6393310187b9142f277736e1eab8
Manav <manav18gadhiya@gmail.com>
2024-03-27 11:41:22 +0530

    Handler added to Notify Processes of the Price Feed

--- price-feed-bot-Manav2808/PriceFeedBot.lua
+++ price-feed-bot-Manav2808/PriceFeedBot.lua
@@ -1,12 +1,15 @@
--- Process ID: MD76snAyJJICvDt2rhhA68zIjPSIYJDKuyQ19yFiTGE
+-- Process ID: 2yXaoxARx1vbh49nwDV3CByC77GjJ4fkhLzp_9IO8zg
 
 _0RBIT = "WSXUI2JjYUldJ7CKq9wE1MGwXs-ldzlUlHOQszwQe0s"
 
+REQUESTS = {}
+
 Handlers.add(
-    "pingpong",
+    "Get Price",
     Handlers.utils.hasMatchingTag("Action", "Get-Price"),
     function(msg)
         local coin_ids = ''
+
         for i in string.gmatch(msg.Tags.IDS, "[^,]+") do
             i = i:match("^%s*(.-)%s*$")
             coin_ids = coin_ids .. i .. '%2C'
@@ -17,6 +20,19 @@ Handlers.add(
             Action = "Get-Real-Data",
             Url = 'https://api.coingecko.com/api/v3/simple/price?ids=' .. coin_ids .. '&vs_currencies=usd',
         })
-        Handlers.utils.reply("Response")(msg)
+
+        table.insert(REQUESTS, msg.From)
+    end
+)
+
+Handlers.add(
+    "Receive Data",
+    Handlers.utils.hasMatchingTag("Action", "Receive-data-feed"),
+    function(msg)
+        ao.send({
+            Target = REQUESTS[0],
+            Data = Inbox[#Inbox].Data
+        })
+        table.remove(REQUESTS, 0)
     end
 )

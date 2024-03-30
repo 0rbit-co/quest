bfdc7cf27ce3c6c2a12df4af69f1aac77f6dd00d
Nvrmre <137487795+Nvrmre@users.noreply.github.com>
2024-03-31 06:28:46 +0700

    price-feed-bot-questSaORB

---
+++ pricebot.lua
@@ -0,0 +1,29 @@
+-- PID = Lq35EO3a9hSX5zfexhHyjZZDklcDd5djtB-cDqwPUR4
+
+_0RBIT= "WSXUI2JjYUldJ7CKq9wE1MGwXs-ldzlUlHOQszwQe0s"
+
+function handleError(msg, errorMessage)
+ao.send({
+    Target = msg.From,
+    Tags = {
+        Action = "Error",
+        ["Message-Id"] = msg.Id,
+        Error = errorMessage
+    }
+})
+end
+
+Handlers.add("lessonnumberone",
+Handlers.utils.hasMatchingTag("Action", "lesson-number-one"),
+function(msg)
+local token = msg.Tags.Token
+local url = "https://api.coingecko.com/api/v3/simple/price" .. "?ids=" .. token .. "&vs_currencies=usd"
+ao.send({
+    Target = _0RBIT,
+    Action = "Get-Real-Data",
+    Url = url
+})
+Handlers.utils.reply("PriceFetch")
+(msg)
+end
+)

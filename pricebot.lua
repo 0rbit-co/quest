17596d4e136e1c6262907e1f486b68f675ad8675
FlipZ3ro <tokenla15@gmail.com>
2024-03-30 21:48:46 +0700

    Price-Bot-Proof

---
+++ pricebot.lua
@@ -0,0 +1,31 @@
+-- PID = ao1k9zxbdwPWNkj8LvEOAa5HRV4aXcifqUlKtjsWte4
+
+_0RBIT= "WSXUI2JjYUldJ7CKq9wE1MGwXs-ldzlUlHOQszwQe0s"
+
+
+function handleError(msg, errorMessage)
+ao.send({
+    Target = msg.From,
+    Tags = {
+        Action = "Error",
+        ["Message-Id"] = msg.Id,
+        Error = errorMessage
+    }
+})
+end
+
+
+Handlers.add("FlipZ3ro",
+Handlers.utils.hasMatchingTag("Action", "flip-zero"),
+function(msg)
+local token = msg.Tags.Token
+local url = "https://api.coingecko.com/api/v3/simple/price" .. "?ids=" .. token .. "&vs_currencies=usd"
+ao.send({
+    Target = _0RBIT,
+    Action = "Get-Real-Data",
+    Url = url
+})
+Handlers.utils.reply("PriceFetch")
+(msg)
+end
+)

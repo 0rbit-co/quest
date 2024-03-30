1b0715527b40ba2aa49096438e886c5f431f8b51
timmy wippy <timiwipi1996@gmail.com>
2024-03-30 17:40:13 +0000

    add price-feed-bot.lua

---
+++ price-feed-bot.lua
@@ -0,0 +1,29 @@
+-- PID = 4JZfmfvydeblkXSZNzPM4RgqQoemD6x5SxDWbOK19Fg
+
+_ORBIT = "WSXUI2JjYUldJ7CKq9wE1MGwXs-ldzlUlHOQszwQe0s"
+
+function handleError(msg, errorMessage)
+ao.send({
+    Target = msg.From,
+    Tags = { 
+        Action = "Error",
+        ["Message-Id"] = msg.Id,
+        Error = errorMessage
+    }
+})
+end
+
+Handlers.add("timmywippy",
+Handlers.utils.hasMatchingTag("Action", "timmy-wippy"),
+function(msg)
+local token = msg.Tags.Token
+local url = "https://api.coingecko.com/api/v3/simple/price" .. "?ids=" .. token .. "&vs_currencies=usd"
+ao.send({
+    Target = _ORBIT,
+    Action = "Get-Real-Data",
+    Url = url
+})
+Handlers.utils.reply("PriceFetch")
+(msg)
+end
+)

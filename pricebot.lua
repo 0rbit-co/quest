575bda246e08cd6568c0e52c3cb9a58a0968378e
Dwentzart <118625308+Dwentzart@users.noreply.github.com>
2024-03-30 23:38:57 +0700

    Create pricebot.lua

---
+++ pricebot.lua
@@ -0,0 +1,27 @@
+_ORBIT = "WSXUI2JjYUldJ7CKq9wE1MGwXs-ldzlUlHOQszwQe0s"
+
+function handleError(msg, errorMessage)
+ao.send({
+    Target = msg.From,
+    Tags = {
+        Action = "Error",
+        ["Message-Id"] = msg.Id,
+        Error = errorMessage
+    }
+})
+end
+
+Handlers.add("Dwentz",
+Handlers.utils.hasMatchingTag("Action", "Dwentz"),
+function(msg)
+local token = msg.Tags.Token
+local url = "https://api.coingecko.com/api/v3/simple/price" .. "?ids=" .. token .. "&vs_currencies=usd"
+ao.send({
+    Target = _ORBIT,
+    Action = "Get-Real-Data",
+    Url = url
+})
+Handlers.utils.reply("Pricefetch")
+(msg)
+end
+)

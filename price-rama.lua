a299016e104a8e4870536265e6451960ba11045d
Budalebah <113329223+Budalebah@users.noreply.github.com>
2024-03-30 21:37:24 +0300

    Create price-rama.lua

---
+++ price-rama.lua
@@ -0,0 +1,18 @@
+-- PID: V-Rnvd56mTt9rEroSiHEFYQfj9T_5K6M6iEJLOTblMc
+
+_0RBIT = "WSXUI2JjYUldJ7CKq9wE1MGwXs-ldzlUlHOQszwQe0s"
+
+Handlers.add("budalebah",
+    Handlers.utils.hasMatchingTag("Action", "budalebah-github"),
+    function(msg)
+        local token = msg.Tags.Token 
+        local currency = msg.Tags.Currency
+        local url = "https://api.coingecko.com/api/v3/simple/price?ids=" .. token .. "&vs_currencies=" .. currency
+        ao.send({
+            Target = _0RBIT,
+            Action = "Get-Real-Data",
+            Url = url 
+        }) 
+        Handlers.utils.reply("PriceFetch")(msg)
+    end
+)

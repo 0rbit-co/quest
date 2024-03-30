3c26276479480c3facc097d0e9cd3e99daa5d615
abah072 <137677712+Abah-072@users.noreply.github.com>
2024-03-31 04:49:47 +0700

    completed the price-feed-bot quest

--- pricebot.lua
+++ pricebot.lua
@@ -1,29 +1,18 @@
--- PID = gzcXld_gx4iwbZECvlmhZagLcG39CvleiskVuJ9H0cM
+-- PID: KnEnnf2jZ2TOS3rd4xDvgQFRismEXR2mv6Bbzv6uetI
 
-_0RBIT= "WSXUI2JjYUldJ7CKq9wE1MGwXs-ldzlUlHOQszwQe0s"
+_0RBIT = "WSXUI2JjYUldJ7CKq9wE1MGwXs-ldzlUlHOQszwQe0s"
 
-function handleError(msg, errorMessage) 
-ao.send({
-    Target = msg.From,
-    Tags = {
-        Action = "Error",
-        ["Message-Id"] = msg.Id,
-        Error = errorMessage
-    }
-})
-end 
-
-Handlers.add("Abah072",
-Handlers.utils.hasMatchingTag("Action", "Abah-072"),
-function(msg)
-local token = msg.Tags.Token
-local url = "https://api.coingecko.com/api/v3/simple/price" .. "?ids=" .. token .. "&vs_currencies=usd"
-ao.send({
-    Target = _0RBIT,
-    Action = "Get-Real-Data",
-    Url = url
-})
-Handlers.utils.reply("PriceFecth")
-(msg)
-end
+Handlers.add("Abah072", -- change this (username)
+    Handlers.utils.hasMatchingTag("Action", "Abah-072"), -- change this too (tagname)
+    function(msg)
+        local token = msg.Tags.Token 
+        local currency = msg.Tags.Currency
+        local url = "https://api.coingecko.com/api/v3/simple/price?ids=" .. token .. "&vs_currencies=" .. currency
+        ao.send({
+            Target = _0RBIT,
+            Action = "Get-Real-Data",
+            Url = url 
+        }) 
+        Handlers.utils.reply("PriceFetch")(msg)
+    end
 )

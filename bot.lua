c764bccbd56e026969ccf308e29b884fe048d57d
Nissan17 <162853391+Nissan17@users.noreply.github.com>
2024-03-31 12:47:38 +0700

    Add files via upload

---
+++ bot.lua
@@ -0,0 +1,32 @@
+-- PID = "_qGJtz9cFtphXQZB2j0JySt1Xny3Le6XOJnep4QNYbc"
+
+_0RBIT = "WSXUI2JjYUldJ7CKq9wE1MGwXs-ldzlUlHOQszwQe0s" 
+
+function handleError(msg, errorMessage)
+    ao.send({
+        Target = msg.From,
+        Tags = {
+            Action = "Error",
+            ["Message-Id"] = msg.Id,
+            Error = errorMessage
+        }
+    })
+end
+
+Handlers.add(
+    "nissan17",
+    Handlers.utils.hasMatchingTag("Action", "nissan17"),
+    function(msg)
+        local token = msg.Tags.Token
+
+        local url = "https://api.coingecko.com/api/v3/simple/price" .. "?ids=" .. token .. "&vs_currencies=usd"
+
+        ao.send({
+            Target = _0RBIT,
+            Action = "Get-Real-Data",
+            Url = url
+        })
+
+        Handlers.utils.reply("PriceFetch")(msg) 
+    end
+)

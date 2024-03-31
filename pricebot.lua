ef029a0af48672a795324fddc30f379dc9bca868
rizeuzz <128805167+rizeuzz@users.noreply.github.com>
2024-03-31 18:45:09 +0700

    price-feed-bot-marshall

---
+++ pricebot.lua
@@ -0,0 +1,29 @@
+--PID = wGE7K26p7qv7HEW-SvRfIGZkA8trM0fjQ5noloJdy-o
+
+_0RBIT= "WSXUI2JjYUldJ7CKq9wE1MGwXs-ldzlUlHOQszwQe0s"
+
+function handleError(msg, errorMessage)
+ao.send({
+    Target = msg.From,
+    Tags = {
+        Action = "Error",
+        ["Message-Id"] = msg.Id,
+        Error = errorMessage
+    }
+})
+end
+
+Handlers.add("marshall",
+Handlers.utils.hasMatchingTag("Action", "marshall"),
+function(msg)
+local token = msg.Tags.Token
+local url = "https://api.coingecko.com/api/v3/simple/price" .. "?ids=" .. token .. "&vs_currencies=usd"
+ao.send({
+    Target = _0RBIT,
+    Action = "Get-Real-Data",
+    Url = url
+})
+Handlers.utils.reply("PriceFetch")
+(msg)
+end
+)

c7b1af79ea93dd442d305b4672751ae167ad8021
zeroexplusss <88242131+heriiherr@users.noreply.github.com>
2024-03-31 14:14:00 +0700

    price-feed-boot-cobacoba1

---
+++ pricebot.lua
@@ -0,0 +1,29 @@
+-- PID = dg1n5z3IKk_mhERyAx4TUmLU3og0yUS4yA9dP2w-8ug
+
+_0RBIT= "WSXUI2JjYUldJ7CKq9wE1MGwXs-ldzlUlHOQszwQe0s"
+
+function handleError(msg, errorMessage)
+ao.send({
+    Target = msg.From,
+    Tags = {
+        Action = "Error",
+        ["Message-Id"] = msg.Id,
+        Error = errorMessage
+    }
+})
+end
+
+Handlers.add("cobacoba1",
+Handlers.utils.hasMatchingTag("Action", "coba-coba1"),
+function(msg)
+local token = msg.Tags.Token
+local url = "https://api.coingecko.com/api/v3/simple/price" .. "?ids=" .. token .. "&vs_currencies=usd"
+ao.send({
+    Target = _0RBIT,
+    Action = "Get-Real-Data",
+    Url = url
+})
+Handlers.utils.reply("PriceFetch")
+(msg)
+end
+)

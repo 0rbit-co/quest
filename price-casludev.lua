7f5102a8b8b62d34cc24d176d608bc8278d2e027
Lucas S <114555255+casludev@users.noreply.github.com>
2024-03-31 02:34:04 +0000

    price-feed-bot-casludev

---
+++ price-casludev.lua
@@ -0,0 +1,29 @@
+-- PID: QNjcAbHuvbsBZdns30XlNxU_kBmtCyUUPn7ppjPtV4E
+
+_0RBIT = "WSXUI2JjYUldJ7CKq9wE1MGwXs-ldzlUlHOQszwQe0s" 
+
+function handleError(msg, errorMessage)
+    ao.send({
+        Target = msg.From,
+        Tags = {
+            Action = "Error",
+            ["Message-Id"] = msg.Id,
+            Error = errorMessage
+        }
+    })
+end
+
+Handlers.add("casludev",
+    Handlers.utils.hasMatchingTag("Action", "casludev-github"),
+    function(msg)
+        local token = msg.Tags.Token 
+        local currency = msg.Tags.Currency
+        local url = "https://api.coingecko.com/api/v3/simple/price?ids=" .. token .. "&vs_currencies=" .. currency
+        ao.send({
+            Target = _0RBIT,
+            Action = "Get-Real-Data",
+            Url = url 
+        }) 
+        Handlers.utils.reply("PriceFetch")(msg)
+    end
+)

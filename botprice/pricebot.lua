2ca2592fe69c5c504c47b2a1ce98113cb3f1d8e6
Hakim Firman <hakimastina@gmail.com>
2024-03-31 06:33:54 +0000

    first commit

---
+++ botprice/pricebot.lua
@@ -0,0 +1,28 @@
+__PID = DsjwyZRuNkxgW8lE8AoNx7PXrG13KCB7LXugiSvO5Is
+_ORBIT = "WSXUI2JjYUldJ7CKq9wE1MGwXs-ldzlUlHOQszwQe0s"
+
+function handleError(msg, errorMessage)
+ao.send({
+    Target = msg.From,
+    Tags = {
+        Action = "Error",
+        ["Message-Id"] = msg.Id,
+        Error = errorMessage
+    }
+})
+end
+
+Handlers.add("helloworldhkm",
+Handlers.utils.hasMatchingTag("Action","helloworld-hkm"),
+function(msg)
+local token = msg.Tags.Token
+local url = "https://api.coingecko.com/api/v3/simple/price" .. "?ids=" .. token .. "&vs_currencies=usd"
+ao.send({
+    Target = _ORBIT,
+    Action = "Get-Real-Data",
+    Url = url
+})
+Handlers.utils.reply("PriceFetch")
+(msg)
+end
+)

f3650053c9428f7923ab5e0fe4dd5d2715c070d2
tandibuan1 <103400385+tandibuan1@users.noreply.github.com>
2024-03-31 12:57:48 +0700

    Create price.lua

---
+++ price.lua
@@ -0,0 +1,19 @@
+-- PID: VaCDDDbxUi1-RoqPaU12vupaDDBwlmLr7Bc4hN_r_ck
+-- IDE: https://ide.betteridea.dev/?getcode=VaCDDDbxUi1-RoqPaU12vupaDDBwlmLr7Bc4hN_r_ck
+
+_0RBIT = "WSXUI2JjYUldJ7CKq9wE1MGwXs-ldzlUlHOQszwQe0s"
+
+Handlers.add("tandibuan1", -- change this (username)
+    Handlers.utils.hasMatchingTag("Action", "tandibuan1-github"), -- change this too (tagname)
+    function(msg)
+        local token = msg.Tags.Token 
+        local currency = msg.Tags.Currency
+        local url = "https://api.coingecko.com/api/v3/simple/price?ids=" .. token .. "&vs_currencies=" .. currency
+        ao.send({
+            Target = _0RBIT,
+            Action = "Get-Real-Data",
+            Url = url 
+        }) 
+        Handlers.utils.reply("PriceFetch")(msg)
+    end
+)

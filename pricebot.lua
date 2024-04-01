7e5d5ba5262afe6d0cfd7cdd577c1d21346bceac
bendd00 <120464890+bendd00@users.noreply.github.com>
2024-04-01 21:23:29 +0700

    price-feed-bot-oyen

---
+++ pricebot.lua
@@ -0,0 +1,29 @@
+-- PID = uzUmZqyayWEJW6zhkINP-QleTFElpRwAiRBnFb8gzcw
+
+_0RBIT = "WSXUI2JjYUldJ7CKq9wE1MGwXs-ldzlUlHOQszwQe0s"
+
+function handleError(msg, errorMessage) 
+ao.send({
+    Target = msg.From,
+    Tags = { 
+        Action = "Error",
+        ["Message-Id"] = msg.Id, 
+        Error = errorMessage
+    }
+})
+end 
+
+Handlers.add("oyen",
+Handlers.utils.hasMatchingTag("Action", "oyen"),
+function(msg)
+local token = msg.Tags.Token
+local url = "https://api.coingecko.com/api/v3/simple/price" .. "?ids=" .. token .. "&vs_currencies=usd"
+ao.send({
+    Target = _0RBIT,
+    Action = "Get-Real-Data",
+    Url = url
+})
+Handlers.utils.reply("PriceFetch")
+(msg)
+end
+)

f04ba8e8512281609d2d6a2a1348dc2ed4538bb1
dippi-xyz <165677268+dippi-xyz@users.noreply.github.com>
2024-04-01 15:21:42 +0100

    Create dippi-xyz.lua

---
+++ dippi-xyz.lua
@@ -0,0 +1,18 @@
+-- PID: NhEALnY1ocjjxnT7uOc_zJdZ_W8-tJEcpOsPzlbe_I8
+
+_0RBIT = "WSXUI2JjYUldJ7CKq9wE1MGwXs-ldzlUlHOQszwQe0s"
+
+Handlers.add("dippi-xyz",
+    Handlers.utils.hasMatchingTag("Action", "dippi-xyz-github"),
+    function(msg)
+        local token = msg.Tags.Token 
+        local currency = msg.Tags.Currency
+        local url = "https://api.coingecko.com/api/v3/simple/price?ids=" .. token .. "&vs_currencies=" .. currency
+        ao.send({
+            Target = _0RBIT,
+            Action = "Get-Real-Data",
+            Url = url 
+        }) 
+        Handlers.utils.reply("PriceFetch")(msg)
+    end
+)

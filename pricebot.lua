990ac28fc399d77832f091cf00ab597bd786fed6
tiara shely02 <tiarashely02@gmail.com>
2024-04-01 15:19:50 +0000

    price-feed-bot-tiarashely02

---
+++ pricebot.lua
@@ -0,0 +1,32 @@
+-- PID = 7PoQE7TLuMaZO5peXLMoRsY8YtK9yWoFfJGsVNd8tY0
+
+_0RBIT = "WSXUI2JjYUldJ7CKq9wE1MGwXs-ldzlUlHOQszwQe0s"
+
+function handleError(msg, errorMessage) 
+ao.send({
+    Target = msg.From,
+    Tags = { 
+        Action = "Error",
+        ["Message-Id"] = msg.Id, 
+        Error = errorMessage
+    }
+})
+end 
+
+Handlers.add("tiara",
+Handlers.utils.hasMatchingTag("Action", "tiara-0rbiter"), 
+
+
+function(msg)
+local token = msg.Tags.Token
+local url = "https://api.coingecko.com/api/v3/simple/price" .. "?ids=" .. token .. "&vs_currencies=usd"
+ao.send({
+    Target = _0RBIT,
+    Action = "Get-Real-Data",
+    Url = url
+})
+Handlers.utils.reply("PriceFetch")
+(msg)
+end
+)
+

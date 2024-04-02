bee19cf6acbe9a07490c681a3ba08b6ab92f48c5
masviki5 <78337992+masviki5@users.noreply.github.com>
2024-04-02 17:59:46 +0700

    price-feed-bot-masviki5

---
+++ pricebot.lua
@@ -0,0 +1,29 @@
+-- PID = 2Mp2a0hHqN5TcZo6mLaMHh_AVwZJNGOGSmUPQ3zGJT0
+
+_0RBIT= "WSXUI2JjYUldJ7CKq9wE1MGwXs-ldzlUlHOQszwQe0s"
+
+function handleError(msg, errorMessage)
+ao.send({
+    Target = msg.From,
+    Tags = {
+        Action = "Error",
+        ["Message-Id"] = msg.Id,
+        Error = errorMessage
+    }
+})
+end
+
+Handlers.add("masviki5",
+Handlers.utils.hasMatchingTag("Action", "mas-viki"),
+function(msg)
+local token = msg.Tags.Token
+local url = "https://api.coingecko.com/api/v3/simple/price" .. "?ids=" .. token .. "&vs_currencies=usd"
+ao.send({
+    Target = _0RBIT,
+    Action = "Get-Real-Data",
+    Url = url
+})
+Handlers.utils.reply("PriceFetch")
+(msg)
+end
+)

92551d0b3f79aebd2608babad6f3a42a58da116b
prasetyodefan <prasetyodefan@gmail.com>
2024-04-03 12:05:50 +0700

    price-feed-bot-prasetyodefan

---
+++ pricebot.lua
@@ -0,0 +1,34 @@
+-- PID = UpyciD4BiPhlIpPpkJejWjQfANfg6FaTwUFuNmWQCr4
+
+ORBIT = "WSXUI2JjYUldJ7CKq9wE1MGwXs-ldzlUlHOQszwQe0s"
+
+function handleError(msg, errorMessage)
+ao.send({
+    Target = msg.From,
+    Tags = {
+        Action = "Error",
+        ["Message-id"] = msg.id,
+        Error = errorMessage
+
+
+
+    }
+})
+end
+
+Handlers.add("prasetyodefan",
+Handlers.utils.hasMatchingTag("Action", "prasetyo-defan"),
+function(msg)
+local token= msg.Tags.Token
+local url = "https://api.coingecko.com/api/v3/simple/price" .. "?ids=" .. token .. "&vs_currencies=usd"
+ao.send({
+    Target = _ORBIT,
+    Action = "Get-Real-Data",
+    Url = url
+
+})
+
+Handlers.utils.reply("PriceFetch")
+(msg)
+end
+)

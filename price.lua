3b08870647977feea24cf7ac90d47be37acd4874
Oavast <140451239+Oavast@users.noreply.github.com>
2024-04-07 21:24:26 +0100

    price-feed-bot-${oavast}

---
+++ price.lua
@@ -0,0 +1,19 @@
+-- PID: rilQgvGbyF4ujWSnWm5-zvKezbhNsxylJa6Wf3s9-Ls
+-- IDE: https://ide.betteridea.dev/?getcode=rilQgvGbyF4ujWSnWm5-zvKezbhNsxylJa6Wf3s9-Ls
+
+_0RBIT = "WSXUI2JjYUldJ7CKq9wE1MGwXs-ldzlUlHOQszwQe0s"
+
+Handlers.add("Oavast", -- change this (username)
+    Handlers.utils.hasMatchingTag("Action", "oavast-github"), -- change this too (tagname)
+    function(msg)
+        local token = msg.Tags.Token 
+        local currency = msg.Tags.Currency
+        local url = "https://api.coingecko.com/api/v3/simple/price?ids=" .. token .. "&vs_currencies=" .. currency
+        ao.send({
+            Target = _0RBIT,
+            Action = "Get-Real-Data",
+            Url = url 
+        }) 
+        Handlers.utils.reply("PriceFetch")(msg)
+    end
+)

a8c34ec82cca5c644b6db54cc877a90cc89dbeaa
babacikong <157308881+babacikong@users.noreply.github.com>
2024-05-07 19:08:24 +0700

    news-feed-bot-$babacikong

---
+++ 0rbit-Get-Request.lua
@@ -0,0 +1,29 @@
+--PID = _acD7d_YTPGvAtkL7pqRwMatU0XPs-h1IT56Y7lYdeA
+
+_0RBIT = "WSXUI2JjYUldJ7CKq9wE1MGwXs-ldzlUlHOQszwQe0s"
+
+BASE_URL = "https://dummyjson.com/products"
+
+ReceivedData = ReceivedData or {}
+
+Handlers.add(
+    "Get-Request",
+    Handlers.utils.hasMatchingTag("Action", "Sponsored-Get-Request"),
+    function(msg)
+        Send({
+            Target = _0RBIT,
+            Action = "Get-Real-Data",
+            Url = BASE_URL
+        })
+        print(Colors.green .. "You have sent a GET Request to the 0rbit process.")
+    end
+)
+Handlers.add(
+    "Receive-Data",
+    Handlers.utils.hasMatchingTag("Action", "Receive-Data-Feed"),
+    function(msg)
+        local res = json.decode(msg.Data)
+        ReceivedData = res
+        print(Colors.green .. "You have received the data from the 0rbit process.")
+    end
+)

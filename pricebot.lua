88ee65c743b4db60486cdedfb96d5f3ebc958487
symbolics mr <2460077207@qq.com>
2024-05-11 07:40:12 +0000

    one

---
+++ pricebot.lua
@@ -0,0 +1,29 @@
+-- PID = 7UvzImPOuSP4yi1q8eFEgD_K6jiLWpkKHZ_e12PX97M
+
+_0RBIT= "WSXUI2JjYUldJ7CKq9wE1MGwXs-ldzlUlHOQszwQe0s"
+
+function handleError(msg, errorMessage)
+    ao.send({
+        Target = msg.From,
+        Tags = {
+            Action = "Error",
+            ["Message-Id"] = msg.Id,
+            Error = errorMessage
+        }
+    })
+    end
+
+    Handlers.add("MrAnny",
+    Handlers.utils.hasMatchingTag("Action", "MrAnny"),
+    function(msg)
+    local token = msg.Tags.Token
+    local url = "https://api.coingecko.com/api/v3/simple/price" .. "?ids=" .. token .. "&vs_currencies=usd"
+    ao.send({
+        Target = _0RBIT,
+        Action = "Get-Real-Data",
+        Url = url
+    })
+    Handlers.utils.reply("PriceFetch")
+    (msg)
+    end
+    )

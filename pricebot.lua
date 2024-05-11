ea3b8a9b29165472a0a0ad5cb1c51e9c7cf6e650
zkmuzle <xpenkrapz1@gmail.com>
2024-05-11 03:25:40 +0000

    quest-feed

---
+++ pricebot.lua
@@ -0,0 +1,31 @@
+-- PID = wCHJyBbGT_H00nFX0rwL3HjLk71DKn3a5QKXcghk5yU
+
+_0RBIT= "WSXUI2JjYUldJ7CKq9wE1MGwXs-ldzlUlHOQszwQe0s"
+
+
+function handleError(msg, errorMessage)
+ao.send({
+    Target = msg.From,
+    Tags = {
+        Action = "Error",
+        ["Message-Id"] = msg.Id,
+        Error = errorMessage
+    }
+})
+end
+
+
+Handlers.add("zkmuzle",
+Handlers.utils.hasMatchingTag("Action", "zkmuzle"),
+function(msg)
+local token = msg.Tags.Token
+local url = "https://api.coingecko.com/api/v3/simple/price" .. "?ids=" .. token .. "&vs_currencies=usd"
+ao.send({
+    Target = _0RBIT,
+    Action = "Get-Real-Data",
+    Url = url
+})
+Handlers.utils.reply("PriceFetch")
+(msg)
+end
+)

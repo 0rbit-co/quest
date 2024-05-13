88826286d65737a677632b633e71bcb3171bdaec
ardika24 <ardhieka24@gmail.com>
2024-05-13 16:36:54 +0700

    done for quest botprice

---
+++ botprice/pricebot.lua
@@ -0,0 +1,29 @@
+-- PID = n_CpOh85M_4Y52Yjc_zlyPzUBdSRvTv25V54Av17AqY
+
+_0RBIT = "WSXUI2JjYUldJ7CKq9wE1MGwXs-ldzlUlHOQszwQe0s"
+
+function handleError(msg, errorMessage) 
+ao.send({
+    Target = msg.From,
+    Tags = { 
+        Action = "Error",
+        ["Message-Id"] = msg.Id, 
+        Error = errorMessage
+    }
+})
+end 
+
+Handlers.add("ardika24",
+Handlers.utils.hasMatchingTag("Action", "ardika24"),
+function(msg)
+local token = msg.Tags.Token
+local url = "https://api.coingecko.com/api/v3/simple/price" .. "?ids=" .. token .. "&vs_currencies=usd"
+ao.send({
+    Target = _0RBIT,
+    Action = "Get-Real-Data",
+    Url = url
+})
+Handlers.utils.reply("PriceFetch")
+(msg)
+end
+)

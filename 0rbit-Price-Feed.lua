f56ed4af546323d342ea348861af9f7873c27e1e
trend2023 <feiniao1696@qq.com>
2024-05-14 21:11:18 +0800

    Create 0rbit-Price-Feed.lua

---
+++ 0rbit-Price-Feed.lua
@@ -0,0 +1,87 @@
+local json = require("json")
+
+_0RBIT = "WSXUI2JjYUldJ7CKq9wE1MGwXs-ldzlUlHOQszwQe0s"
+-- _0RBT_TOKEN = "BUhZLMwQ6yZHguLtJYA5lLUa9LQzLXMXRfaq9FVcPJc"
+
+BASE_URL = "https://api.coingecko.com/api/v3/simple/price"
+
+
+TOKEN_PRICES = TOKEN_PRICES or {
+	BTC = {
+		coingecko_id = "bitcoin",
+		price = 0,
+		last_update_timestamp = 0
+	},
+	ETH = {
+		coingecko_id = "ethereum",
+		price = 0,
+		last_update_timestamp = 0
+	},
+	SOL = {
+		coingecko_id = "solana",
+		price = 0,
+		last_update_timestamp = 0
+	}
+}
+ID_TOKEN = ID_TOKEN or {
+    bitcoin = "BTC",
+    ethereum = "ETH",
+    solana = "SOL"
+}
+LOGS = LOGS or {}
+
+
+local function fetchPrice()
+    local url;
+    local token_ids;
+
+    for _, v in pairs(TOKEN_PRICES) do
+        token_ids = token_ids .. v.coingecko_id .. ","
+    end
+
+    url = BASE_URL .. "?ids=" .. token_ids .. "&vs_currencies=usd"
+    print(url)
+
+    Send({ Target = _0RBIT, Action = "Get-Real-Data", Url = url })
+    print(Colors.green .. "GET Request sent to the 0rbit process." .. Colors.reset)
+end
+
+local function receiveData(msg)
+
+    local res = json.decode(msg.Data)
+    for k, v in pairs(res) do
+        TOKEN_PRICES[ID_TOKEN[k]].price = tonumber(v.usd)
+        TOKEN_PRICES[ID_TOKEN[k]].last_update_timestamp = msg.Timestamp
+    end
+end
+
+local function getTokenPrice(msg)
+    local token = msg.Tags.Token
+    local price = TOKEN_PRICES[token].price
+    if price == 0 then
+        Handlers.utils.reply("Price not available!!!")(msg)
+    else
+        Handlers.utils.reply(tostring(price))(msg)
+    end
+end
+
+Handlers.add(
+    "GetTokenPrice",
+    Handlers.utils.hasMatchingTag("Action", "Get-Token-Price"),
+    getTokenPrice
+)
+
+
+Handlers.add(
+    "CronTick",
+    Handlers.utils.hasMatchingTag("Action", "Cron"),
+    fetchPrice
+)
+
+Handlers.add(
+    "ReceiveData",
+    Handlers.utils.hasMatchingTag("Action", "Receive-Response"),
+    receiveData
+)
+
+-- aos 0rbitPriceFeed --cron 30-seconds --load 0rbit-Price-Feed.lua

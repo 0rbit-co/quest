048a1a54de6a56efdf99f6e5a4d2691a1631b7c4
just1lovewrl <90997099+just1lovewrl@users.noreply.github.com>
2024-05-14 15:49:57 +0800

    Update price-feed-bot-just1lovewrl.lua

--- price-feed-bot-just1lovewrl.lua
+++ price-feed-bot-just1lovewrl.lua
@@ -1,102 +1,55 @@
--- PID U_GoThvB1w4GpadwjmUsJcqjGU6plbKGkvGYsphcrYw
-
-
 local json = require("json")
 
--- processId of the 0rbit process.
-_0RBIT = "WSXUI2JjYUldJ7CKq9wE1MGwXs-ldzlUlHOQszwQe0s"
-_0RBT_TOKEN = "BUhZLMwQ6yZHguLtJYA5lLUa9LQzLXMXRfaq9FVcPJc"
+_ORBIT = "WSXUI2JjYUldJ7CKq9wE1MGwXs-ldzlUlHOQszwQe0s"
 
--- Base URL for Binance API
-BASE_URL = "https://data-api.binance.vision/api/v3/ticker"
-FEE_AMOUNT = "1000000000000" -- 1 $0RBT
-
---[[
-    Mapping to store the Token Details
-]]
-TOKEN_PRICES = TOKEN_PRICES or {
-    BTC = {
-        symbol = "BTC",
-        price = 0,
-        last_update_timestamp = 0
-    },
-    ETH = {
-        symbol = "ETH",
-        price = 0,
-        last_update_timestamp = 0
-    },
-    SOL = {
-        symbol = "SOL",
-        price = 0,
-        last_update_timestamp = 0
-    }
-}
-
---[[
-    Function to get the price of a token
-]]
-function getTokenPrice(msg)
-    local token = msg.Tags.Token
-    local price = TOKEN_PRICES[token].price
-    if price == 0 then
-        Handlers.utils.reply("Price not available!!!")(msg)
-    else
-        Handlers.utils.reply(tostring(price))(msg)
-    end
+function handleError(msg, errorMessage)
+    ao.send({
+        Target = msg.From,
+        Tags = {
+            Action = "Error",
+            ["Message-Id"] = msg.Id,
+            Error = errorMessage
+        }
+    })
 end
 
---[[
-    Function to create a GET request on 0rbit to fetch the token prices
-]]
-function fetchPrice()
-    for symbol, details in pairs(TOKEN_PRICES) do
-        local url = BASE_URL .. "?symbol=" .. details.symbol .. "USDT&windowSize=1m&type=FULL"
-
-        Send({
-            Target = _0RBT_TOKEN,
-            Action = "Transfer",
-            Recipient = _0RBIT,
-            Quantity = FEE_AMOUNT,
-            ["X-Url"] = url,
-            ["X-Action"] = "Get-Real-Data"
+Handlers.add("just1love",
+    Handlers.utils.hasMatchingTag("Action", "Sponsored-Get-Request"),
+    function(msg)
+        local token = msg.Tags.Token
+        if not token then
+            handleError(msg, "Token not provided")
+            return
+        end
+        
+        local url = "https://api.coingecko.com/api/v3/simple/price?ids=" .. token .. "&vs_currencies=usd"
+        ao.send({
+            Target = _ORBIT,
+            Action = "Get-Real-Data",
+            Url = url
         })
-        print(Colors.green .. "GET Request sent to the 0rbit process for " .. symbol)
+        print("Pricefetch request sent for " .. token)
     end
-end
-
---[[
-    Function to update the token prices in the TOKEN_PRICES table
-]]
-function receiveData(msg)
-    local res = json.decode(msg.Data)
-    local symbol = res.symbol:sub(1, -5)  -- Remove 'USDT' from the symbol name
-    TOKEN_PRICES[symbol].price = tonumber(res.lastPrice)
-    TOKEN_PRICES[symbol].last_update_timestamp = msg.Timestamp
-end
-
---[[
-    Handler to get the token price from the TOKEN_PRICES table
-]]
-Handlers.add(
-    "GetTokenPrice",
-    Handlers.utils.hasMatchingTag("Action", "Get-Token-Price"),
-    getTokenPrice
 )
 
---[[
-    CRON Handler to fetch the token prices using 0rbit in a defined interval.
-]]
-Handlers.add(
-    "CronTick",
-    Handlers.utils.hasMatchingTag("Action", "Cron"),
-    fetchPrice
-)
-
---[[
-    Handler to receive the data from the 0rbit process
-]]
-Handlers.add(
-    "ReceivingData",
+Handlers.add("ReceiveData",
     Handlers.utils.hasMatchingTag("Action", "Receive-Response"),
-    receiveData
+    function(msg)
+        print("Received data: " .. msg.Data)
+        local res = json.decode(msg.Data)
+        local token = msg.Tags.Token
+        if res[token] and res[token].usd then
+            ao.send({
+                Target = msg.From,
+                Tags = {
+                    Action = "Price-Response",
+                    ["Message-Id"] = msg.Id,
+                    Price = res[token].usd
+                }
+            })
+            print("Price of " .. token .. " is " .. res[token].usd)
+        else
+            handleError(msg, "Failed to fetch price")
+        end
+    end
 )

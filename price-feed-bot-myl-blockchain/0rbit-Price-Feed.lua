5b733f59cc93fce67ec5268425a3de503f1b17ff
myl-blockchain <118060519+myl-blockchain@users.noreply.github.com>
2024-05-18 13:19:19 +0300

    Update 0rbit-Price-Feed.lua

--- price-feed-bot-myl-blockchain/0rbit-Price-Feed.lua
+++ price-feed-bot-myl-blockchain/0rbit-Price-Feed.lua
@@ -15,7 +15,7 @@ function handleError(msg, errorMessage)
     })
 end
 
-Handlers.add("https://github.com/myl-blockchain",
+Handlers.add("myl-blockchain",
     Handlers.utils.hasMatchingTag("Action", "Sponsored-Get-Request"),
     function(msg)
         local token = msg.Tags.Token

1927a2e2714764f3db787a791097c8673c333649
korcano <116563799+korcano@users.noreply.github.com>
2024-05-18 21:03:26 +0300

    Update 0rbit-Price-Feed.lua

--- price-feed-bot-eltaz/0rbit-Price-Feed.lua
+++ price-feed-bot-eltaz/0rbit-Price-Feed.lua
@@ -15,7 +15,7 @@ function handleError(msg, errorMessage)
     })
 end
 
-Handlers.add("eltaz",
+Handlers.add("korcano",
     Handlers.utils.hasMatchingTag("Action", "Sponsored-Get-Request"),
     function(msg)
         local token = msg.Tags.Token

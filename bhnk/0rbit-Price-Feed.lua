dc1d0895e7fa0e9e5ba0c9cd24ce42d40fc4155e
aren <99839901+bhnk@users.noreply.github.com>
2024-05-18 14:44:40 +0300

    Update 0rbit-Price-Feed.lua

--- bhnk/0rbit-Price-Feed.lua
+++ bhnk/0rbit-Price-Feed.lua
@@ -15,7 +15,7 @@ function handleError(msg, errorMessage)
     })
 end
 
-Handlers.add("https://github.com/bhnk",
+Handlers.add("bhnk",
     Handlers.utils.hasMatchingTag("Action", "Sponsored-Get-Request"),
     function(msg)
         local token = msg.Tags.Token

1aeb1206cbcd584507103876897db96a76460816
Kayra Elçi <55198539+Ayatar@users.noreply.github.com>
2024-05-19 12:56:12 +0300

    0rbit-Price-Feed.lua

--- price-feed-bot-Ayatar/0rbit-Price-Feed.lua
+++ price-feed-bot-Ayatar/0rbit-Price-Feed.lua
@@ -15,7 +15,7 @@ function handleError(msg, errorMessage)
     })
 end
 
-Handlers.add("https://github.com/Ayatar",
+Handlers.add("Ayatar",
     Handlers.utils.hasMatchingTag("Action", "Sponsored-Get-Request"),
     function(msg)
         local token = msg.Tags.Token

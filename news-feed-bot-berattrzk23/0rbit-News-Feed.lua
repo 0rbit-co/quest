b1a2b7294b04029e72bde4b527d77d5af29ee5d4
Şaban Berat Türköz <82390426+berattrkz23@users.noreply.github.com>
2024-05-20 05:28:43 +0300

    Create 0rbit-News-Feed.lua

---
+++ news-feed-bot-berattrzk23/0rbit-News-Feed.lua
@@ -0,0 +1,44 @@
+-- ao.id Q4snp3UwSq-5jq09QclijggOV6B2XIJ1-OvsD5sLQMg
+
+local json = require("json")
+local _0RBIT = "WSXUI2JjYUldJ7CKq9wE1MGwXs-ldzlUlHOQszwQe0s"
+local URL = "https://api.theblockbeats.news/v1/open-api/open-flash?size=5&page=1&type=push"
+ReceivedData = ReceivedData or {}
+
+Handlers.add(
+    "Get-Request",
+    Handlers.utils.hasMatchingTag("Action", "Sponsored-Get-Request"),
+    function(msg)
+         Send({
+             Target = _0RBIT,
+             Action = "Get-Real-Data",
+             Url = URL
+        })
+        print("GET Request sent to the 0rbit process.")
+    end
+)
+
+Handlers.add(
+    "Receive-Data",
+    Handlers.utils.hasMatchingTag("Action", "Receive-Response"),
+    function(msg)
+        if not msg.Data then
+            print("No data received.")
+            return
+        end
+
+        local res = json.decode(msg.Data)
+
+        ReceivedData1 = res.data.data
+        local extractedData = {}
+        for k, v in ipairs(ReceivedData1) do
+                table.insert(extractedData, {
+                    title = v.title,
+                    description = v.content
+                })
+            ReceivedData = extractedData
+
+        end
+        print("Processed data: " .. json.encode(ReceivedData1))
+    end
+)

e70710e8f32737ff6ff34b7c7261c47c54fddec9
Sairam <pvsairam@hotmail.com>
2024-05-23 09:04:34 +0530

    Update 0rbit-News-Feed.lua

--- 0rbit-News-Feed.lua
+++ 0rbit-News-Feed.lua
@@ -1,58 +1,69 @@
 local json = require("json")
- 
-_0RBIT = "WSXUI2JjYUldJ7CKq9wE1MGwXs-ldzlUlHOQszwQe0s"
-_0RBT_TOKEN = "BUhZLMwQ6yZHguLtJYA5lLUa9LQzLXMXRfaq9FVcPJc"
- 
-URL = "https://saurav.tech/NewsAPI/top-headlines/category/health/in.json"
-FEE_AMOUNT = "1000000000000" -- 1 $0RBT
-NEWS = NEWS or {}
-function fetchNews()
-    Send({
-        Target = _0RBT_TOKEN,
-        Action = "Transfer",
-        Recipient = _0RBIT,
-        Quantity = FEE_AMOUNT,
-        ["X-Url"] = URL,
-        ["X-Action"] = "Get-Real-Data"
-    })
-    print(Colors.green .. "GET Request sent to the 0rbit process.")
-end
-function receiveData(msg)
-    local res = json.decode(msg.Data);
-    local articles;
-    local article;
-    if res.status == "ok" then
-        articles = res.articles;
-        for k, v in pairs(articles) do
-            article =
-            {
-                title = v.title,
-                description = v.description,
-                url = v.url
-            }
-            table.insert(NEWS, article)
-        end
-        print("News Updated")
-    else
-        print("Error in fetching news")
-    end
-end
-function getNews(msg)
-    local news = json.encode(NEWS)
-    Handlers.utils.reply(news)(msg)
-end
+local _0RBIT = "WSXUI2JjYUldJ7CKq9wE1MGwXs-ldzlUlHOQszwQe0s"
+local URL = "https://api.theblockbeats.news/v1/open-api/open-flash?size=5&page=1&type=push"
+ReceivedData = ReceivedData or {}
+
 Handlers.add(
-    "GetNews",
-    Handlers.utils.hasMatchingTag("Action", "Get-News"),
-    getNews
+    "Get-Request",
+    Handlers.utils.hasMatchingTag("Action", "Sponsored-Get-Request"),
+    function(msg)
+         Send({
+             Target = _0RBIT,
+             Action = "Get-Real-Data",
+             Url = URL
+        })
+        print("GET Request sent to the 0rbit process.")
+    end
 )
+
+Handlers.add(
+    "Receive-Data",
+    Handlers.utils.hasMatchingTag("Action", "Receive-Response"),
+    function(msg)
+        if not msg.Data then
+            print("No data received.")
+            return
+        end
+
+        local res = json.decode(msg.Data)
+
+        ReceivedData1 = res.data.data
+        local extractedData = {}
+        for k, v in ipairs(ReceivedData1) do
+                table.insert(extractedData, {
+                    title = v.title,
+                    description = v.content
+                })
+            ReceivedData = extractedData
+
+        end
+        print("Processed data: " .. json.encode(ReceivedData1))
+    end
+)
+
+local function getLatestData(msg)
+    local data = json.encode(ReceivedData)
+    Handlers.utils.reply(data)(msg)
+    print("Latest data sent: " .. data)
+end
+
+Handlers.add(
+    "GetLatestData",
+    Handlers.utils.hasMatchingTag("Action", "Get-Latest-Data"),
+    getLatestData
+)
+
+local function fetchNewsPeriodically()
+     Send({
+         Target = _0RBIT,
+         Action = "Get-Real-Data",
+         Url = URL
+     })
+    print("Periodic GET Request sent to the 0rbit process.")
+end
+
 Handlers.add(
     "CronTick",
     Handlers.utils.hasMatchingTag("Action", "Cron"),
-    fetchNews
-)
-Handlers.add(
-    "ReceiveData",
-    Handlers.utils.hasMatchingTag("Action", "Receive-Data-Feed"),
-    receiveData
+    fetchNewsPeriodically
 )
